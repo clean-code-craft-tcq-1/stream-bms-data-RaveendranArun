3b9bb01b9163487818c0cae6dab56ec6e26cd2b3
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-06-22 05:08:52 +0530

    Update BMS_TestReceiver.cpp

--- Test_Receiver/BMS_TestReceiver.cpp
+++ Test_Receiver/BMS_TestReceiver.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "../Test_Sender/catch.hpp"
-#include "BMS_Receiver/dataBmsRx.h"
+#include "../BMS_Receiver/dataBmsRx.h"
 
 TEST_CASE(" Test case to check if data is read from sender to Reciever is success ") 
 {
