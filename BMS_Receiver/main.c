f4e4b33a62663c9ec4fb1953fbfbcdbcb0e72c22
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-06-22 05:23:43 +0530

    Create main.c

---
+++ BMS_Receiver/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+#include <string.h>
+#include "dataBmsRx.h"
+
+int main()
+{
+  /* calling test main to check if data is streamed from sender to reciever */
+  (void)testMain();
+}
