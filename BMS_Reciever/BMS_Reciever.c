0ea51e7e0b42d2d9c154531cc254704a81a50cb6
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-06-22 03:30:53 +0530

    Add files via upload

---
+++ BMS_Reciever/BMS_Reciever.c
@@ -0,0 +1,163 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+#include <string.h>
+#include "BMS_Reciever.h"
+
+/* global variable declaration */
+batteryReading_st bmsTempSocData;
+dataBms_St inputSt;
+
+retBmsStatus_en performBmsRead(char *txBmsReadPtr)
+{
+	retBmsStatus_en retStatus = ERROR_STATUS;
+	/* check if pointer is valid */
+	if(txBmsReadPtr!=NULL)
+	{
+		scanf("%s",txBmsReadPtr);
+		retStatus = OK_STATUS;
+	}
+	return retStatus;
+}
+
+retBmsStatus_en prepareConvAtoF(txBmsReadPtr)
+{
+	retBmsStatus_en retStatus = ERROR_STATUS;
+	int lengthOfString = 0x00;
+	int loopCntr = 0;
+	int cntrval = 0;
+	lengthOfString = strlen(txBmsReadPtr);
+	/* add new line and null to the end if string */
+    txBmsReadPtr[lengthOfString] = '\n';
+    txBmsReadPtr[lengthOfString + 1] = '\0';
+	bmsTempSocData.stringSize = lengthOfString;
+    for(loopCntr = 0;txBmsReadPtr[loopCntr]!='\0';loopCntr++)
+    {
+    if((((txBmsReadPtr[loopCntr]>='0')&&(txBmsReadPtr[loopCntr]<='9'))||(txBmsReadPtr[loopCntr] == '.')))
+    {
+       inputSt.y = convertAsciiToFloat(&txBmsReadPtr[loopCntr]);
+       loopCntr += inputSt.cntr;
+       inputSt.cntr=0;
+	   updateBmsDataValues(cntrval);
+	   cntrval++;
+       printf("%f", inputSt.y);
+    }
+	retStatus = OK_STATUS;
+    }
+	return retStatus;
+}
+
+retBmsStatus_en convertAsciiToFloat(char *txBmsinfoRead)
+{
+  int loopCntr = 0;
+  int num = 0, num2 = 0, point_ch = 0, x = 0, decPointNum = 1; 
+  float f2 = 0.00, f3 = 0.00;
+
+  point_ch = checkNumPoint(&txBmsinfoRead[loopCntr]);
+	
+  for (; (((txBmsinfoRead[loopCntr]>='0')&&(txBmsinfoRead[loopCntr]<='9'))||(txBmsinfoRead[loopCntr] == '.')); loopCntr++) 
+  {
+    if (loopCntr < point_ch)
+    {
+        num = num * 10 + (txBmsinfoRead[loopCntr] - 48);
+    }
+    else if (loopCntr == point_ch) 
+    /* do nothing */;
+    else
+    {
+      num2 = num2 * 10 + (txBmsinfoRead[loopCntr] - 48);
+      ++x;
+    }
+    inputSt.cntr = loopCntr;
+  }
+  
+  /* it takes 10 if it has 1 digit ,100 if it has 2 digits in mantissa*/
+    decPointNum = setDeciPoint(x);
+  f2 = num2 / (float) decPointNum;
+  printf("f2=%f\n",f2);
+  f3 = num + f2;
+  return f3;	
+}
+
+int checkNumPoint(char *addrChPtr)
+{
+	int loopCntr = 0;
+	int pnt_ch = 0;
+	for (loopCntr; addrChPtr[loopCntr]; loopCntr++)
+    if (addrChPtr[loopCntr] == '.') 
+	{
+      pnt_ch = loopCntr;
+      break;
+    }
+	return pnt_ch;
+}
+
+int setDeciPoint(int pointNum)
+{
+  int loopCntr = 0;
+  int decPntNum = 1; 
+  /* it takes 10 if it has 1 digit ,100 if it has 2 digits in mantissa*/
+  for (loopCntr = 1; loopCntr <= pointNum; loopCntr++) 
+    decPntNum = decPntNum * 10;
+  return decPntNum;
+}
+
+void updateBmsDataValues(int arrIndx)
+{
+	if((arrIndx&1)!=1)
+	{
+		bmsTempSocData.batteryTempearature[bmsTempSocData.numOfData] = inputSt.y;
+	}
+	else
+	{
+		bmsTempSocData.batterySoc[bmsTempSocData.numOfData] = inputSt.y;
+		bmsTempSocData.numOfData+=1;
+	}
+}
+
+retBmsStatus_en RxBmsData()
+{
+	retBmsStatus_en retStatus = ERROR_STATUS;
+	char txBmsDataRead[100];
+	/* perform Read from console or sender to update the txBmsDataRead bufffer */
+	retStatus = performBmsRead(txBmsDataRead);
+	/* convert BMS data of Sender from ascii to float for comparison if success status is OK */
+	if(retStatus != ERROR_STATUS)
+	{
+		retStatus = prepareConvAtoF(txBmsDataRead);
+	}
+	
+	return retStatus;
+}
+
+void checkTempValIsMax(float *maxValue, float maxRange)
+{
+	for(  int loopCntr = 0;loopCntr<(bmsTempSocData.numOfData);loopCntr)
+	{
+		if(maxValue[loopCntr] >= maxRange)
+		{
+			printf("Max range is breached\n",maxValue[loopCntr]);
+		}
+	}
+}
+void checkValIsMin(float *minValue, float minRange)
+{
+	for(  int loopCntr = 0;loopCntr<(bmsTempSocData.numOfData);loopCntr)
+	{
+		if(minValue[loopCntr]<=minRange)
+		{
+			printf("Min range is breached\n",minValue[loopCntr]);
+		}
+	}
+}
+int main()
+{
+	retBmsStatus_en retStatus = ERROR_STATUS;
+	retStatus = RxBmsData();
+	checkValIsMax(&bmsTempSocData.batteryTempearature[0], TEMPERATURE_MAX);
+	checkValIsMax(&bmsTempSocData.batterySoc[0], SOC_MAX);
+	checkValIsMin(&bmsTempSocData.batteryTempearature[0], TEMPERATURE_MIN);
+	checkValIsMin(&bmsTempSocData.batterySoc[0], SOC_MIN);
+	
+}
---
+++ BMS_Reciever/BMS_Reciever.h
@@ -0,0 +1,44 @@
+#ifndef DATABMSTX_H_
+#define DATABMSTX_H_
+
+/* Macro Declaration */
+#define ARRINDEXSIZE 1000
+#define TEMPERATURE_MAX (float)45.0
+#define TEMPERATURE_MIN (float)0.0
+
+#define SOC_MAX (float)80.0
+#define SOC_MIN (float)20.0
+
+/* Enum declarations */
+typedef enum
+{
+  OK_STATUS,
+  ERROR_STATUS
+}retBmsStatus_en;
+
+typedef struct
+{
+    float y;
+    int cntr;
+}dataBms_St;
+/* Structure declarations */
+typedef struct
+{
+  float batteryTempearature[ARRINDEXSIZE];
+  float batterySoc[ARRINDEXSIZE];
+  int numOfData;
+}batteryReading_st;
+
+/* Function declaration */
+
+void updateBmsDataValues(int arrIndx);
+int checkNumPoint(char *addrChPtr);
+int setDeciPoint(int pointNum);
+retBmsStatus_en prepareConvAtoF(char *txBmsReadPtr);
+retBmsStatus_en convertAsciiToFloat(char *txBmsinfoRead);
+retBmsStatus_en performBmsRead(char *txBmsReadPtr);
+void checkTempValIsMax(float *maxValue, float maxRange);
+void checkValIsMin(float *minValue, float minRange);
+retBmsStatus_en RxBmsData(void);
+
+#endif  /* DATABMSTX_H_ */
