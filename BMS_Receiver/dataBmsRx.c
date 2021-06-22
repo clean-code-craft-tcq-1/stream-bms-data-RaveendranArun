182664a28a608cfd6e637180dbc42344953ada3c
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-06-22 06:25:06 +0530

    Update dataBmsRx.c

--- BMS_Receiver/dataBmsRx.c
+++ BMS_Receiver/dataBmsRx.c
@@ -32,6 +32,7 @@ retBmsStatus_en prepareConvAtoF(char *txBmsReadPtr)
     txBmsReadPtr[lengthOfString] = '\n';
     txBmsReadPtr[lengthOfString + 1] = '\0';
 	bmsTempSocData.stringSize = lengthOfString;
+	printf("txBmsReadPtr = %f\n", txBmsReadPtr);
     for(loopCntr = 0;txBmsReadPtr[loopCntr]!='\0';loopCntr++)
     {
     if((((txBmsReadPtr[loopCntr]>='0')&&(txBmsReadPtr[loopCntr]<='9'))||(txBmsReadPtr[loopCntr] == '.')))
